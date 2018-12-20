d8c75241c0e90630a655da23ec4e1c375dbbc6a0
dpslwk <dps.lwk@gmail.com>
2018-12-20 11:02:38 +0000

    Get summer note from https source

--- resources/views/trustees/emailMembers/draft.blade.php
+++ resources/views/trustees/emailMembers/draft.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @push('scripts')
-  <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-bs4.css" rel="stylesheet">
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-bs4.js"></script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-bs4.css" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-bs4.js"></script>
   <script src="https://unpkg.com/docs-soap@1.2.0/dist/docs-soap.min.js"></script>
   <script>
     $(document).ready(function() {
@@ -36,7 +36,7 @@
 <div class="container">
   <p>Use the form below to darft an email to all members</p>
   <p>Note the formating template will add "Hello name", at the top and "This email was sent to you as a current member of Nottingham Hackspace" as a footer</p>
-  <form class="form-group" id="tustesse-emailMembers-draft-from" role="form" method="POST" action="{{  route('trustees.email-members.review') }}"> 
+  <form class="form-group" id="tustesse-emailMembers-draft-from" role="form" method="POST" action="{{  route('trustees.email-members.review') }}">
     @csrf
 
     <div class="form-group">
@@ -48,7 +48,7 @@
       </span>
       @endif
     </div>
-    
+
     <div class="form-group">
       <label for="emailContent" class="form-label">Content</label>
       <textarea id="emailContent" name="emailContent" class="form-control" rows="10" required>{{ old('emailContent', $emailContent) }}</textarea>
@@ -66,6 +66,6 @@
         </button>
       </div>
     </div>
-  </form> 
+  </form>
 </div>
 @endsection
