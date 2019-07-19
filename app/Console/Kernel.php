beecaf05e4c8883fe5735c942c9b423d645c941b
dpslwk <dps.lwk@gmail.com>
2019-07-19 08:34:13 +0100

    TeamEmail: reminder has moved over to HMS2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new EmailTeamReminder)->weeklyOn(2, '7:27');
+        // $schedule->job(new EmailTeamReminder)->weeklyOn(2, '7:27');
         // $schedule->job(new SnackspaceDebtJob)->daily();
     }
 
